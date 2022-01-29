6e23ab95b270e015ba65b878b2ddff9a441bc643
Payton Thomas <paytonco@gmail.com>
2022-01-28 21:58:06 -0700

    addded wind effects

--- util.py
+++ util.py
@@ -2,7 +2,13 @@ import numpy as np
 from scipy.ndimage import convolve
 
 def findKernel(state,wind,humidity,precip): 
-    kernel = np.ones((3,3))
+    kernel = np.zeros((3,3))
+    kernel[0,0] = wind/15
+    kernel[0,1] = 0.5
+    kernel[1,0] = 0.5
+    kernel[1,2] = 0.5
+    kernel[2,1] = 0.5
+    
     return kernel
 
 def maskConvolve(state,kernel,mask):
