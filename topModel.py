514fd91a594080a378768c219a639cb1df838c24
Payton Thomas <paytonco@gmail.com>
2022-01-29 16:34:50 -0700

    added burnTimes for each zone in the city to topModel

--- topModel.py
+++ topModel.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import model
 
 initialState = np.zeros((100,200))
-initialState[50,50] = 0.1
+initialState[49,57] = 0.1
+#city is at initialState[46:54,125:142]
+burnTimes = np.zeros((8,17))
 
 tempHigh = np.array([94,87,86,97,98,89,92,96,98,99,103,103,97,91])
 tempLow = np.array([60,56,55,59,61,61,57,58,59,64,65,64,62,57])
@@ -13,3 +15,11 @@ humid = np.array([49.2,57.1,55.2,41.5,40.0,44.1,45.1,42.5,45.3,38.1,34.3,35.3,50
 
 testModel = model.firemodel(initialState, tempHigh, tempLow, precip, wind, humid)
 
+for i in range(336):
+    testModel.timeStep()
+    for j in range(8):
+        for k in range(17):
+            if testModel.state[j+46,k+125] != 0:
+                burnTimes[j,k] = i + 1
+
+fig, ax1 = plt.imshow(burnTimes,cmap=plt.get_cmap('inferno'))
