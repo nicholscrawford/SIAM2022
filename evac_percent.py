8b170f603a30a6f02333d8824afeeb34d2a08815
jukyll530 <98627037+jukyll530@users.noreply.github.com>
2022-01-29 19:04:04 -0700

    Add files via upload

---
+++ evac_percent.py
@@ -0,0 +1,15 @@
+from typing import List, Union
+
+e1 = [2109. 3136, 1623, 573, 1205, 1277, 408, 1689]
+e2 = e1 * 3
+e3 = e2 * 3
+
+
+def percentevac(e1):
+    SUM = sum(e1)
+    ind_pos = e1[1]
+    for ind_pos in el:
+        percent = e1[1] / SUM
+        print (percent)
+        ind_pos += 1
+
