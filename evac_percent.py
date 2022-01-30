590bf410739351debb2b1039f39bc151aa5acd2a
jukyll530 <98627037+jukyll530@users.noreply.github.com>
2022-01-29 20:21:31 -0700

    Add files via upload
    Revised percent calculator to make CDF

--- evac_percent.py
+++ evac_percent.py
@@ -1,15 +1,18 @@
-from typing import List, Union
+import math
+import numpy as np
 
-e1 = [2109. 3136, 1623, 573, 1205, 1277, 408, 1689]
-e2 = e1 * 3
-e3 = e2 * 3
+e1 = [2109, 3136, 1623, 573, 1205, 1277, 408, 1689]
+e2 = [22712, 34864, 17599, 6119, 13332, 13400, 4636, 17850]
+e3 = [76165, 115761, 57483, 20105, 43531, 44517, 15493, 58874]
+
+e1 = np.array(e1)
+e1perc = e1/sum(e1)
+
+newlist = np.zeros(e1perc.shape)
+for i in range(len(e1perc)):
+    newlist[i] = sum(e1perc[0:i+1])
 
 
-def percentevac(e1):
-    SUM = sum(e1)
-    ind_pos = e1[0]
-    for ind_pos in el:
-        percent = e1[1] / SUM
-        print (percent)
-        ind_pos += 1
+print(e1perc)
+print(newlist)
 
